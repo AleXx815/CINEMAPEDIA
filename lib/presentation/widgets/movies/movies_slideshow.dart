b7f3a7feb36e8006d780f35f5e139e040fe974ba
David Alexi <davgoga815@gmail.com>
2023-05-09 19:02:56 -0500

    inicio de carrusel y cardswiper implementados

---
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:card_swiper/card_swiper.dart';
+import 'package:cinemapadia/domain/entities/movie.dart';
+
+class MoviesSlideShow extends StatelessWidget {
+  final List<Movie> movies;
+
+  const MoviesSlideShow({
+    super.key,
+    required this.movies,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 210,
+      width: double.infinity,
+      child: Swiper(
+        viewportFraction: 0.8,
+        scale: 0.6,
+        autoplay: true,
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          final movie = movies[index];
+          return _Slide(movie: movie);
+        },
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

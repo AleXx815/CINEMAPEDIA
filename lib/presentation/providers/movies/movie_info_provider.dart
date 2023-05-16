840d471fe5a8d23ac802e7744e6d24058a9d0564
David Alexi <davgoga815@gmail.com>
2023-05-16 16:47:59 -0500

    caché local

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,34 @@
+import 'package:cinemapadia/domain/entities/movie.dart';
+import 'package:cinemapadia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieInfoProvider = StateNotifierProvider((ref) {
+  final movieRepository = ref.watch(movieRepositoryProvider);
+  return MovieMapNotifer(getMovie: movieRepository.getMovieById);
+});
+
+/*
+VOY A CREAR UN MAPA
+{
+  key(se muestre el id de la película): Movie()(se muestre la instancia de la película),
+}
+
+
+*/
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifer extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+
+  MovieMapNotifer({
+    required this.getMovie,
+  }) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) return;
+
+    final movie = await getMovie(movieId);
+
+    state = {...state, movieId: movie};
+  }
+}

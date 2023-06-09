caa22f9c3be3a99b85cdfc3161c10f948256c409
AleXx815 <davgoga815@gmail.com>
2023-06-09 12:04:23 -0500

    functional search bar without buildresults

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,19 +1,40 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+import '../../domain/entities/movie.dart';
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies,
+  });
+
   @override
   String get searchFieldLabel => "Buscar película";
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      const Text("BuildActions"),
+      // if (query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          onPressed: () => query = "",
+          icon: const Icon(Icons.clear),
+        ),
+      ),
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text("buildLeading");
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(Icons.arrow_back_ios_new_outlined),
+    );
   }
 
   @override
@@ -23,6 +44,20 @@ class SearchMovieDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text("buildSuggestions");
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+
+            return ListTile(title: Text(movie.title));
+          },
+        );
+      },
+    );
   }
 }
